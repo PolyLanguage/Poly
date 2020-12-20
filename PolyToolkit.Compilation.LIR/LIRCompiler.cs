301e82aff8e8e6a13ead252b9c84ecdadeb29a1c
qrai <sergeikab77@gmail.com>
2020-12-20 10:39:30 +0400

    lir proj init

---
+++ PolyToolkit.Compilation.LIR/LIRCompiler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LLVMSharp;
+namespace PolyToolkit.Compilation.LIR
+{
+    public class LIRCompiler : ICompilerService
+    {
+        public PolyProgram Program { get; set; }
+
+        public LIRCompiler(PolyProgram program)
+        {
+            Program = program;
+        }
+
+        #region Methods
+        public void CompileProgram(PolyProgram program)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CompileSingleFile(PolyCodefile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateByteCode(PolyProgram program)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateCode(PolyProgram program)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ PolyToolkit.Compilation.LIR/LIRGen.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LLVMSharp;
+
+using PolyToolkit.Parsing;
+using PolyToolkit.Parsing.Ast;
+namespace PolyToolkit.Compilation.LIR
+{
+    public class LIRGen
+    {
+        public PolyProgram Program { get; set; }
+        public LIRGen(PolyProgram program)
+        {
+            Program = program;
+        }
+
+        public void Generate()
+        {
+            LLVMBuilderRef builder = LLVM.CreateBuilder();
+
+            LLVM.LinkInMCJIT();
+            LLVM.InitializeX86TargetInfo();
+            LLVM.InitializeX86Target();
+            LLVM.InitializeX86TargetMC();
+
+            foreach(PolyCodefile file in Program.Files)
+            {
+                LLVMModuleRef module = GenerateModule(file.FileName.Replace(".poly", ""), file.CodeTree);
+            }
+
+            //TODO: return LIR
+        }
+
+        private LLVMModuleRef GenerateModule(string name,CodeTree code)
+        {
+            LLVMModuleRef module = LLVM.ModuleCreateWithName(name);
+
+            // Create a function pass manager for this engine
+            LLVMPassManagerRef passManager = LLVM.CreateFunctionPassManagerForModule(module);
+
+            // Set up the optimizer pipeline.  Start with registering info about how the
+            // target lays out data structures.
+            // LLVM.DisposeTargetData(LLVM.GetExecutionEngineTargetData(engine));
+
+            // Provide basic AliasAnalysis support for GVN.
+            LLVM.AddBasicAliasAnalysisPass(passManager);
+            // Promote allocas to registers.
+            LLVM.AddPromoteMemoryToRegisterPass(passManager);
+            // Do simple "peephole" optimizations and bit-twiddling optzns.
+            LLVM.AddInstructionCombiningPass(passManager);
+            // Reassociate expressions.
+            LLVM.AddReassociatePass(passManager);
+            // Eliminate Common SubExpressions.
+            LLVM.AddGVNPass(passManager);
+            // Simplify the control flow graph (deleting unreachable blocks, etc).
+            LLVM.AddCFGSimplificationPass(passManager);
+            LLVM.InitializeFunctionPassManager(passManager);
+
+            //generate
+            foreach(IAstNode node in code.Childs)
+            {
+                VisitAndGenerate(node,module);
+            }
+
+            return module;
+        }
+
+        private void VisitAndGenerate(IAstNode node,LLVMModuleRef module)
+        {
+            if(node is ClassNode)
+            {
+                //generate class
+
+                //generate class members (method/field/constructor)
+                foreach (IAstNode clChild in ((ClassNode)node).Body.Childs)
+                    VisitAndGenerate(node,module);
+            }
+        }
+    }
+}
